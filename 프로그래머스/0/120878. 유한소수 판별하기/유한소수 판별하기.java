22e9bf08b884e7e47f0782b0c99b03c040ab43be
UBokchi <105459234+Ubokchi@users.noreply.github.com>
2025-09-09 18:04:04 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int a, int b) {
+        int g = gcd(a, b);
+        b /= g;
+
+        while (b % 2 == 0) {
+            b /= 2;
+        }
+        while (b % 5 == 0) {
+            b /= 5;
+        }
+
+        return (b == 1) ? 1 : 2;
+    }
+
+    private int gcd(int x, int y) {
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+        return x;
+    }
+}
