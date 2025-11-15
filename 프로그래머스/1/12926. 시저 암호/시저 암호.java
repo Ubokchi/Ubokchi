b16d21c78300437382e07a2febfc48b046357732
UBokchi <105459234+Ubokchi@users.noreply.github.com>
2025-11-15 23:57:41 +0900

    [level 1] Title: 시저 암호, Time: 0.99 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -5,18 +5,15 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
-            // 공백이면 그대로 추가
             if (ch == ' ') {
                 answer.append(' ');
                 continue;
             }
 
-            // 대문자 A~Z
             if (ch >= 'A' && ch <= 'Z') {
                 char shifted = (char)((ch - 'A' + n) % 26 + 'A');
                 answer.append(shifted);
             }
-            // 소문자 a~z
             else if (ch >= 'a' && ch <= 'z') {
                 char shifted = (char)((ch - 'a' + n) % 26 + 'a');
                 answer.append(shifted);
