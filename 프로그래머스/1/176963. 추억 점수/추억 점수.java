69a5b2e6a0362392eeedb99e602ad90c5f9f68f6
UBokchi <105459234+Ubokchi@users.noreply.github.com>
2025-11-27 19:04:08 +0900

    [level 1] Title: 추억 점수, Time: 1.72 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        int[] result = new int[photo.length];
+
+        for (int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for (String person : photo[i]) {
+                sum += map.getOrDefault(person, 0);
+            }
+            result[i] = sum;
+        }
+
+        return result;
+    }
+}
