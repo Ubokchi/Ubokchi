3ea9f85bd8d1be65c5ff00cf161eb5383b536d46
UBokchi <105459234+Ubokchi@users.noreply.github.com>
2025-12-03 19:25:43 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int a, int b) {
+        int[] monthDays = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+
+        int totalDays = 0;
+
+        for (int i = 0; i < a - 1; i++) {
+            totalDays += monthDays[i];
+        }
+
+        totalDays += (b - 1);
+
+        return days[totalDays % 7];
+    }
+}
